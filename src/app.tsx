330099ed0d913b844cad282b79699ca1e26b8273
Andreas Friedel <afe-gmdg@gmx.de>
2020-12-07 19:27:04 +0100

    Added react-dom for comparing

--- src/app.tsx
+++ src/app.tsx
@@ -1,23 +1,38 @@
 import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { debounce } from "./common";
 import { getCUIContext } from "./cui";
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const div = document.getElementById("app") as HTMLDivElement;
 const context = getCUIContext(canvas, 0.122, 0.122, 0.122);
-window.addEventListener("resize", context.resize);
+window.addEventListener("resize", () => debounce(context.resize));
 
 type AppProps = {
 };
 
-const App: React.FC<AppProps> = (_props) => {
+const CuiApp: React.FC<AppProps> = (_props) => {
   // const { children } = props;
   const [isEnabled, setIsEnabled] = React.useState(true);
 
   return (
     <cuiPanel>
-      <cuiButton disabled={isEnabled}>This is a cuiButton</cuiButton>
-      <cuiButton onClick={() => setIsEnabled(!isEnabled)}>Enable/Disable other cuiButton</cuiButton>
+      <cuiButton disabled={!isEnabled}>This is a cui button.</cuiButton>
+      <cuiButton onClick={() => setIsEnabled(!isEnabled)}>Enable/Disable other cui button.</cuiButton>
     </cuiPanel>
   );
 };
 
-context.render(<App />);
+const ReactApp: React.FC<AppProps> = (_props) => {
+  const [isEnabled, setIsEnabled] = React.useState(true);
+
+  return (
+    <div>
+      <button type="button" disabled={!isEnabled}>This is a html button.</button>
+      <button type="button" onClick={() => setIsEnabled(!isEnabled)}>Enable/Disable other html button.</button>
+    </div>
+  );
+};
+
+context.render(<CuiApp />);
+ReactDOM.render(<ReactApp />, div);
--- src/common.ts
+++ src/common.ts
@@ -1,3 +1,17 @@
+type DeboundeCallback = () => void;
+const debounceMap = new Map<DeboundeCallback, number>();
+export function debounce(cb: DeboundeCallback) {
+  let handle = debounceMap.get(cb);
+  if (handle !== undefined) {
+    window.clearTimeout(handle);
+  }
+  handle = window.setTimeout(() => {
+    debounceMap.delete(cb);
+    cb();
+  }, 100);
+  debounceMap.set(cb, handle);
+}
+
 export function delay(ms: number) {
   return new Promise<void>((resolve) => {
     window.setTimeout(resolve, ms);
